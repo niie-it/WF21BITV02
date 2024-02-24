626c83a08a83369143bd59c1580b2d319e73249e
HIENLTH 2023 <hyhien@gmail.com>
2024-02-24 09:30:47 +0700

    Demo Mang Trai cay

---
+++ BTMangDong/Program.cs
@@ -0,0 +1,27 @@
+﻿// Nhập tên các loại trái cây, dừng khi không nhập hoặc stop
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.InputEncoding = Encoding.UTF8;
+List<string> mang = new List<string>();
+while (true)
+{
+	Console.Write("Tên trái cây: ");
+	string tenTrai = Console.ReadLine().Trim();
+	if (tenTrai == "" || tenTrai.ToUpper() == "STOP")
+	{
+		break;
+	}
+	if (mang.Contains(tenTrai, StringComparer.OrdinalIgnoreCase))
+	{
+		Console.WriteLine("Đã có " + tenTrai); continue;
+	}
+	mang.Add(tenTrai);
+	Console.WriteLine(string.Join(", ", mang));
+	Console.WriteLine($"Count={mang.Count}, Cap={mang.Capacity}");
+}
+
+mang.Sort();
+Console.WriteLine(string.Join(", ", mang));
+mang.Reverse();
+Console.WriteLine(string.Join(", ", mang));
---
+++ DemoForEach/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoForEach
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] tenSvs = { "Tèo", "Tý", "Mận", "Đào", "Cam" };
+			foreach(string ten in tenSvs)
+			{
+				Console.WriteLine(ten);
+			}
+		}
+	}
+}
