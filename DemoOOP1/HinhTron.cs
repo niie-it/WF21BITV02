ea0b4a65c093e57de0cdcaf72b716254aa602bca
HIENLTH 2023 <hyhien@gmail.com>
2024-03-02 08:41:46 +0700

    Demo OOP basic

---
+++ DemoOOP1/HinhTron.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoOOP1
+{
+	public class HinhTron
+	{
+		private double _BanKinh;
+		public double BanKinh
+		{
+			get { return _BanKinh; }
+			set
+			{
+				if (value < 0)
+				{
+					throw new Exception("Ban kinh am");
+				}
+				_BanKinh = value;
+			}
+		}
+
+		public double DienTich { get; set; }
+		public double ChuVi { get; set; }
+		public HinhTron(double bk = 1)
+		{
+			BanKinh = bk;
+		}
+		public void TinhDienTichChuVi()
+		{
+			DienTich = BanKinh * BanKinh * Math.PI;
+			ChuVi = 2 * BanKinh * Math.PI;
+			Console.Write($"S={DienTich} P={ChuVi}");
+		}
+		public override string ToString()
+		{
+			return $"Tron co R = {BanKinh}, S={DienTich} P={ChuVi}";
+		}
+	}
+}
---
+++ DemoOOP1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using DemoOOP1;
+
+var x = new Diem();
+//Console.WriteLine($"{x.HoanhDo}, {x.TungDo}");
+x.Xuat();
+var y = new Diem(7, 11);
+Console.WriteLine($"{y.HoanhDo}, {y.TungDo}");
+Console.WriteLine(y);
+
+// Demo hình tròn
+var ht1 = new HinhTron();
+Console.WriteLine(ht1);
+var ht2 = new HinhTron(3);
+Console.WriteLine(ht2);
+var ht3 = new HinhTron(-8);
+Console.WriteLine(ht3);
