c08014845985134c61eb6ee52729677fe6f42eaa
HIENLTH 2023 <hyhien@gmail.com>
2024-03-04 12:09:28 +0700

    Lab 04 Bai tap 01

---
+++ Lab04BT01_Tong/Lab04BT01_Tong/Form1.cs
@@ -0,0 +1,28 @@
+﻿namespace Lab04BT01_Tong
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnTinh_Click(object sender, EventArgs e)
+		{
+			if (int.TryParse(txtN.Text, out int N) && N > 0)
+			{
+				int S = 0;
+				for (int i = 0; i < N; i++)
+				{
+					S += i + 1;
+				}
+				txtTong.Text = S.ToString();
+			}
+			else
+			{
+				MessageBox.Show("Nhập sai rùi", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				txtN.Focus();
+			}
+		}
+	}
+}
